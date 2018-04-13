6511101ced661d0a0ce55ff8536ba7c21aea46c0
Melhjingoy David <melhdavid@gmail.com>
2018-04-13 17:50:51 -0400

    Add files via upload

---
+++ datastore.js
@@ -0,0 +1,122 @@
+const STORE = [
+    {
+      question: `Who is the founder of Marvel?`,
+      answers: [
+        `Martin Goodman`,
+        `Stan Lee`,
+        `Peter Parker`,
+        `Walt Disney`
+        ],
+      correctAnswer: `Martin Goodman`,
+    //   icon: 'https://i.imgur.com/Rue4wCj.png',
+    //   alt: 'tent icon'
+    },
+    {
+      question: `Who has the ability to control metals?`,
+      answers: [
+        `Moving-Guy`,
+        `Magnet-Man`,
+        `Magneto`,
+        `Mario`
+        ],
+        correctAnswer: `Magneto`,
+        // icon: 'https://i.imgur.com/iQ0FBhB.png',
+        // alt: 'shield for 10 essentials icon'
+    },
+    {
+      question: `Which superhero is not part of the Marvel Universe?`,
+      answers: [
+        `Iron Man`,
+        `Captain America`,
+        `Wolverine`,
+        `Batman`
+        ],
+        correctAnswer: `Batman`,
+        // icon: 'https://i.imgur.com/uXukVjk.png',
+        // alt: 'weight icon'
+    },
+    {
+      question: `Who told Peter Parker (Spiderman) “With great power comes great responsibility?`,
+      answers: [
+        `Aunt Mae`,
+        `Thor`,
+        `Uncle Ben`,
+        `Bruce Banner`
+        ],
+      correctAnswer: `Uncle Ben`,
+    //   icon: 'https://i.imgur.com/i0Q515j.png',
+    //   alt: 'bear paw icon'
+    },
+    {
+      question: `Peter Parker works as a photographer for:`,
+      answers: [
+        `The New York Times`,
+        `The Post`,
+        `The Daily Bugle`,
+        `The Daily Planet`
+        ],
+      correctAnswer: `The Daily Bugle`,
+    //   icon: 'https://i.imgur.com/LHRMcBd.png',
+    //   alt: 'hat icon'
+    },
+    {
+      question: `The Thing is a member of which team?`,
+      answers: [
+        `X-men`,
+        `The Liberators`,
+        `The Fantastic Four`,
+        `The Power Rangers`
+        ],
+      correctAnswer: `The Fantastic Four`,
+    //   icon: 'https://i.imgur.com/XEwf3tK.png',
+    //   alt: 'water bottle icon'
+    },
+    {
+      question: `Who is the main villain in the 2018 Marvel’s Avengers Infinity War movie?`,
+      answers: [
+      `Thanos`,
+      `Braniac`,
+      `Darkseid`,
+      `Lex Luthor`
+      ],
+      correctAnswer: `Thanos`,
+    //   icon: 'https://i.imgur.com/Kk9t3qL.png',
+    //   alt: 'stream icon'
+    },
+    {
+      question: `Who is the crowned prince of Asgard?`,
+      answers: [
+        `Black Panther`,
+        `Daredevil`,
+        `Thor`,
+        `Vision`
+      ],
+      correctAnswer: `Thor`,
+    //   icon: 'https://i.imgur.com/A2cWS88.png',
+    //   alt: 'compass icon'
+    },
+    {
+      question: `What state in the USA Peter Parker, Jessica Jones, Matt Murdock and Luke Cage reside?`,
+      answers: [
+        `Virginia`,
+        `Gotham City`,
+        `New York`,
+        `New Jersey`
+      ],
+      correctAnswer: `New York`,
+    //   icon: 'https://i.imgur.com/VwFRlXL.png',
+    //   alt: 'bear head icon'
+    },
+    {
+      question: `What kind of metal Wolverine’s skeleton plated?`,
+      answers: [
+        `Titanium`,
+        `Tungsten`,
+        `Material X`,
+        `Adamantium`
+    ],
+    correctAnswer: `Adamantium`,
+    // icon: 'https://i.imgur.com/YdcTVPW.png',
+    // alt: 'skull and crossbones icon'
+    }
+];
---
+++ index.js
@@ -0,0 +1,166 @@
+
+let questionNumber = 0;
+let score = 0;
+let win = 0;
+let lose = 0;
+//generate question html
+
+function generateQuestion() {
+    if (questionNumber < STORE.length) {
+        return `<div class="question-${questionNumber} question-form">
+      <h2>${STORE[questionNumber].question}</h2>
+      <form>
+      
+        <p class="card-text">
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="${STORE[questionNumber].answers[0]}" checked>
+                <label class="form-check-label" for="exampleRadios1">
+                ${STORE[questionNumber].answers[0]}
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="${STORE[questionNumber].answers[1]}">
+                <label class="form-check-label" for="exampleRadios2">
+                ${STORE[questionNumber].answers[1]}
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="${STORE[questionNumber].answers[2]}">
+                <label class="form-check-label" for="exampleRadios2">
+                ${STORE[questionNumber].answers[2]}
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="${STORE[questionNumber].answers[3]}">
+                <label class="form-check-label" for="exampleRadios2">
+                ${STORE[questionNumber].answers[3]}
+                </label>
+            </div>
+        </p>
+        <a href="#" class="btn btn-primary finished answered">Submit</a>
+      </form>
+      </div>`;
+    } else {
+        restartQuiz();
+        $('.question-number').text(10)
+    }
+}
+
+/**hides and removes cards */
+function startQuiz() {
+    $('.question-answer-form, .correction, .finish-line').hide();
+    $('.quiz-start').on('click', '.start-button', function (event) {
+        $('.quiz-start').remove();
+        $('.question-answer-form').show();
+        $('.question-number').text(1);
+    });
+}
+
+// render question in DOM
+function renderQuestion() {
+    $('.question-answer-form').html(generateQuestion());
+}
+
+//user selects answer on submit run user feedback
+function userSelectAnswer() {
+    $('.finished').on('click', function (event) {
+        event.preventDefault();
+        let selected = $('input:checked');
+        let answer = selected.val();
+        let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+        if (answer === correctAnswer) {
+            ifAnswerIsCorrect();
+            answerFeedback();
+        } else {
+            ifAnswerIsWrong();
+            answerFeedback();
+        }
+    });
+}
+
+function answerFeedback() {
+    let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+    $('form').hide();
+    $('.corrected').text(`The right answer was ${correctAnswer}`);
+    $('.correction').show();
+}
+
+//user feedback for correct answer
+function userAnswerFeedbackCorrect() {
+    $('.feed-back-title').text('Awesome!');
+}
+
+//user feedback for wrong answer
+function userAnswerFeedbackWrong() {
+    $('.feed-back-title').text('Sorry!');
+}
+
+
+// gives the result of the answered question and updates that scores
+function ifAnswerIsCorrect() {
+    userAnswerFeedbackCorrect();
+    win++;
+    $('.total-win').text(win);
+}
+
+function ifAnswerIsWrong() {
+    userAnswerFeedbackWrong();
+    lose++;
+    $('.total-lose').text(lose);
+}
+
+function changeQuestionNumber() {
+    questionNumber++;
+    $('.question-number').text(questionNumber + 1);
+}
+
+
+//what happens when the user clicks next
+function renderNextQuestion() {
+    $('.container').on('click', '.next', function (event) {
+        $('.correction').hide();
+        $('form').show();
+        changeQuestionNumber();
+        renderQuestion();
+        userSelectAnswer();
+
+        if (questionNumber >= 10) {
+            $('form').remove();
+            $('.finish-line').show();
+            // $('.resultScore').text(`Congratulations!! your score is ${win}!!!`);
+            if (win >= 8) {
+                $('.resultScore').text(`Congratulations! You are Super! You got ${win}/10`);
+            } else if (score < 8 && score >= 5) {
+                $('.resultScore').text(`Rusty but you made it! You got ${win}/10`);
+            } else {
+                $('.resultScore').text(`You need to sharpen your powers! You got ${win}/10`);
+            }
+        }
+    });
+}
+
+function changeScore() {
+    score++;
+}
+
+//restart quiz function - reloads page to start quiz over
+function restartQuiz() {
+    $('.finish-line').on('click', '.restart', function (event) {
+        location.reload();
+    });
+}
+
+
+
+
+
+
+//run quiz functions
+function createQuiz() {
+    startQuiz();
+    renderQuestion();
+    userSelectAnswer();
+    renderNextQuestion();
+}
+
+$(createQuiz);
